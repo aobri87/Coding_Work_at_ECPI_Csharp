ecc9365c93da5324136c804a81b467bc949700bb
aobri87 <72810973+aobri87@users.noreply.github.com>
2021-11-09 10:43:45 -0500

    Add files via upload

---
+++ MidtermExtraCredit/MidtermExtraCredit/CustomName.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MidtermExtraCredit
+{
+    public partial class CustomName : Form
+    {
+        public CustomName()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label5.Text = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            label5.Text = textBox2.Text + ", " + textBox1.Text;
+        }
+    }
+}
