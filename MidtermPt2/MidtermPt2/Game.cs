b7c9e5361201d4caa8cf0f7b0e08b857f405ace1
aobri87 <72810973+aobri87@users.noreply.github.com>
2021-11-09 10:44:49 -0500

    Add files via upload

---
+++ MidtermPt2/MidtermPt2/Game.cs
@@ -0,0 +1,63 @@
+﻿// Andrew O'Brien
+// 28 Oct 2020
+// Midterm Exam Part 2
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidtermPt2
+{
+    public class Game
+    {
+        private int quantityOfGame;
+        private decimal pricePerGame;
+        // auto implemented GameID, GameDescription, QuantityOfGame, and PricePerGame
+        public string GameID { get; set; }
+        public string GameDescritpion { get; set; }
+        public int QuantityOfGame
+        {
+            get{return quantityOfGame;}
+            // if statment to prevent negative numbers
+            set{if (value >= 0)
+                { quantityOfGame = value; }
+            } // end set
+        } // end QuantityOfGame
+        public decimal PricePerGame
+        {
+            get{return pricePerGame;}
+            // if statment to prevent negative numbers
+            set{if (value >= 0)
+                { pricePerGame = value; }
+            }// end set
+        }// end PricePerGame
+
+        // initializes constructor
+        public Game(string id, string description, int quantity, decimal price)
+        {
+            GameID = id;
+            GameDescritpion = description;
+            QuantityOfGame = quantity;
+            PricePerGame = price;
+
+        } // end constructor
+
+        public void GameTotalAmount()
+        {
+            decimal total = QuantityOfGame * PricePerGame;
+            Console.WriteLine("Game Total Amount: ${0}\n", total);// total amount of purchaced games
+        } // end GameTotalAmount
+
+        // Display recipt message
+        public void DisplayMessage()
+        {
+            // use auto implemented properties for message information
+            Console.WriteLine("Original game information\n");
+            Console.WriteLine("Game ID: {0} ", GameID); // id of game
+            Console.WriteLine("Game Description: {0} ", GameDescritpion);// string of description of name
+            Console.WriteLine("Game Quantitity: {0}", QuantityOfGame);// number of games
+            Console.WriteLine("Game Price: ${0}", PricePerGame);// price per a game
+            GameTotalAmount(); // displays the GameTotalAmount method
+        }// end DisplayMessage
+    }// end class Game
+} // end namespace MidtermPt2
---
+++ MidtermPt2/MidtermPt2/GameTest.cs
@@ -0,0 +1,30 @@
+﻿// Andrew O'Brien
+// 28 Oct 2020
+// Midterm Exam Part 2
+
+using System;
+
+namespace MidtermPt2
+{
+    public class GameTest
+    {
+        // main method begins program execution
+        public static void Main(string[] args)
+        {
+            Game game1 = new Game("1234", "Mario", 2, (decimal)55.95);
+
+            game1.DisplayMessage(); // displays game1 information using DisplayMessage
+                                    // changes info for game1 to new information
+            Console.WriteLine("\nNew game information \n");
+            Console.Write("Game ID: "); // prompt user input game id
+            game1.GameID = Console.ReadLine();// new GameID
+            Console.Write("Game Description: ");// prompt user input for description
+            game1.GameDescritpion = Console.ReadLine();// new GameDescription
+            Console.Write("Game Quantity: "); // prompt user input for quantity
+            game1.QuantityOfGame = Convert.ToInt32(Console.ReadLine());// new GameQuantity
+            Console.Write("Game Price: $");// prompt user input for price
+            game1.PricePerGame = Convert.ToDecimal(Console.ReadLine());// new GamePrice
+            game1.GameTotalAmount(); // displays the GameTotal for the changed information
+        }// end main
+    }// end class GameTest
+}// end namespace MidtermPt2
